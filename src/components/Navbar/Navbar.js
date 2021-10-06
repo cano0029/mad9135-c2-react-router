d30de0efa27740cc2e4d5cb89adfed2888c6f274
Johanna Cano <cano0029@algonquinlive.com>
2021-10-05 22:39:57 -0400

    Add links to navbar and simple styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
+
 const Navbar = (props) => {
   return (
     <div className="navbar">
-      <p>This is the navbar page</p>
+      <NavLink activeClassName="active" to="/" exact>
+        Home
+      </NavLink>
+      <NavLink activeClassName="active" to="/users">
+        Users
+      </NavLink>
+      <NavLink activeClassName="active" to="/addresses">
+        Address
+      </NavLink>
+      <NavLink activeClassName="active" to="/404">
+        Nowhere
+      </NavLink>
     </div>
   );
 };
