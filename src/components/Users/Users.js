f297a27c12f0d5805e3f5630339cb54493461957
Johanna Cano <cano0029@algonquinlive.com>
2021-10-05 23:58:48 -0400

    Make simple fetch from random user api

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,16 +1,28 @@
 import { useState } from 'react';
 
 const Users = (props) => {
-  const [userList, setUserList] = useState([]);
+  const [list, setList] = useState([]);
+
+  async function fetchData() {
+    // TO DO: change url to meet assignment requirements
+    let url = 'https://randomuser.me/api/?results=20';
+    let resp = await fetch(url);
+    let data = await resp.json();
+    console.log(data.results);
+    setList(data.results);
+  }
 
   return (
     <div className="users">
-      <h1>Random Users</h1>
+      <h1 onClick={fetchData}>Random Users</h1>
       <p>This is the random users page</p>
-
-      {!userList.length && <p>There are no users yet</p>}
-      {userList.map((user) => (
-        <p>{user}</p>
+      {!list.length && <p>There are no users yet</p>}
+      {list.map((user) => (
+        <div key={user.name.last + user.cell}>
+          <p>{user.name.first + ' ' + user.name.last}</p>
+          <p>{user.email}</p>
+          <p>{user.cell}</p>
+        </div>
       ))}
     </div>
   );
