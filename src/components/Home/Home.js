e354e469cbd83c6f2ca3e3227bbc958f3bb214b5
Johanna Cano <cano0029@algonquinlive.com>
2021-10-05 23:01:05 -0400

    Add button component in home page that is when clicked shows instructions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const Home = (props) => {
+  const [image, setImage] = useState('insert image');
+  function showInstructions() {
+    setImage('I am your instructions I will tell you to do a lot of things!');
+  }
   return (
     <div className="home">
       <h1 className="greeting"> howdy()</h1>
       <p>Welcome to josRandom Playground</p>
+      <p onClick={showInstructions}>button</p>
+      <p>{image}</p>
     </div>
   );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+
 const Users = (props) => {
+  const [userList, setUserList] = useState([]);
+
   return (
     <div className="users">
       <h1>Random Users</h1>
       <p>This is the random users page</p>
+
+      {!userList.length && <p>There are no users yet</p>}
+      {userList.map((user) => (
+        <p>{user}</p>
+      ))}
     </div>
   );
 };
