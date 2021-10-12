add7bf8aaac89ed213a7facf86f8dba429593bcd
Johanna Cano <cano0029@algonquinlive.com>
2021-10-12 19:21:20 -0400

    Change font sizes and edit instruction text on the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,19 @@
-import ghost from './ghost.png';
 import './home.css';
 import { useState } from 'react';
 
 const Home = (props) => {
   const [instructions, setInstructions] = useState('');
   function showInstructions() {
-    setInstructions(`I am your instructions I will tell you to do a lot of things! Nullam
-    eget felis eget nunc lobortis mattis aliquam faucibus. Nam at lectus
-    urna duis convallis convallis tellus id interdum. Orci nulla
-    pellentesque dignissim enim. Est lorem ipsum dolor sit amet. Commodo
-    quis imperdiet massa tincidunt nunc. Auctor augue mauris augue neque
-    gravida in fermentum et sollicitudin.`);
+    setInstructions(
+      `I am your instructions I will tell you to do a lot of things!
+      First, to see my list of randomly generated users, go the User Page. 
+      While you are there, you will be able to navigate to see each user's Details
+      Page by clicking on their user card. Finally, if you wish to see detailed address
+      information of my random users, simply navigate to the Address Page. There, you
+      can also navigate to a user's Details Page by clicking on their profile picture 
+      or name. I hope that wasn't too confusing... ENJOY! 
+      `
+    );
   }
   return (
     <main className="home-container">
@@ -19,13 +22,11 @@ const Home = (props) => {
           <span>howdy</span>()
         </h1>
         <p>
-          Welcome to josRandom Playground. I'm using the Random Generator API to
-          show you random and fake data. Read the instructions to get started.
+          Welcome to josRandom Playground! Here, I am using the Random Generator
+          API to show you random and fake data. Read the instructions to get
+          started.
         </p>
         <button onClick={showInstructions}>Show Instructions</button>
-      </div>
-      <div className="img-container">
-        <img src={ghost} alt="ghost" />
       </div>
       <div className="instructions">
         <p>{instructions}</p>
